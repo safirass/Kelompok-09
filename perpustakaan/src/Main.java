f92ba6260afb3c8e8d328cfe35652c7ae5111620
Kishirou <129802795+Kishirou@users.noreply.github.com>
2023-04-04 14:09:58 +0700

    Add files via upload

---
+++ perpustakaan/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args)
+    {
+
+        // Creating an empty HashMap
+        HashMap<Integer, String> hash_map = new HashMap<Integer, String>();
+
+        // Mapping string values to int keys
+        hash_map.put(10, "Geeks");
+        hash_map.put(15, "4");
+        hash_map.put(20, "Geeks");
+        hash_map.put(25, "Welcomes");
+        hash_map.put(30, "You");
+
+        // Displaying the HashMap
+        System.out.println("Initial Mappings are: " + hash_map);
+
+        // Inserting existing key along with new value
+        String returned_value = (String)hash_map.put(50, "All");
+
+        // Verifying the returned value
+        System.out.println("Returned value is: " + returned_value);
+
+        // Displaying the new map
+        System.out.println("New map is: " + hash_map);
+    }
+}
